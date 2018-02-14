1b7793b40d61b4983c1220dafaa2f573e42850a2
sunantha2727 <tha-fay@hotmail.co.th>
2018-02-14 11:42:03 +0700

    Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,36 +1,53 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-private loginModel model = new loginModel();
-
-@FXML
-private Label dbStatus;
-@FXML
-private Label loginStatus;
-@FXML
-private TextField username;
-@FXML
-private PasswordField password;
-@FXML
-private Button btnLogin;
-
+    private  loginModel model = new loginModel();
+    //import object from scence builder
+    @FXML
+    private Label dbStatus;
+    @FXML
+    private Label loginStatus;
+    @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+    @FXML
+    private Button btnLogin;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-    if (this.model.isDatabaseConnection()){
-        this.dbStatus.setText("Connection to DB");
+        if (this.model.isDatabaseConnection()) {
+            this.dbStatus.setText("Connectec to DB");
+        } else {
+            this.dbStatus.setText("Not Connect to DB");
+        }
     }
-    else {
-        this.dbStatus.setText("Not Connect to DB");
-    }
+    @FXML
+    public void Login (ActionEvent event){
+//        System.out.println(username.getText());
+//        System.out.println(password.getText());
 
-    }
+        try{
+            if (this.model.isLogin(username.getText(), password.getText())){
+                Stage stage = (Stage) this.btnLogin.getScene().getWindow();
+                //stage.close();
+                loginStatus.setText("Welcome to Our System");
+
+            }else {
+                loginStatus.setText("Your username or password is invalid");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }//Login
+
 }//class
--- src/sample/loginModel.java
+++ src/sample/loginModel.java
@@ -3,6 +3,8 @@ package sample;
 import dbUtil.dbConnection;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class loginModel {
@@ -19,5 +21,26 @@ public class loginModel {
     }
     public  boolean isDatabaseConnection(){
         return  this.connection != null;
-    }
+    }//isDatabaseConnection
+    public boolean isLogin(String user, String pass) throws SQLException {
+        PreparedStatement pr = null;
+        ResultSet rs= null;
+        String sql="select * from user where username=? and password =?";
+        try {
+            pr = this.connection.prepareStatement(sql);
+            pr.setString(1,user);
+            pr.setString(2,pass);
+            rs = pr.executeQuery();
+            if (rs.next()){
+                return  true;
+            }
+            return false;
+        }catch (SQLException ex){
+            ex.printStackTrace();
+            return false;
+        }finally {
+            pr.close();
+            rs.close();
+        }
+    }//isLogin
 }//class
